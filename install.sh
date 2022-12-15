2597bea69c2fb503e40b30f4e2d69af90e917f54
MaggoShton <Maggoshton@gmail.com>
2022-12-15 15:25:14 +0500

    Переписал README.md; Добавил поддержку pacman в install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y python3 python3-venv python3-pip git
+if command -v apt &> /dev/null; then
+    sudo apt install -y python3 python3-venv python3-pip git
+elif command -v pacman &> /dev/null; then
+    sudo pacman -S --noconfirm python3 python3-venv python3-pip git
+else
+    echo "Your distribution is currently not supported"
+    exit
+fi
 git clone https://github.com/holy-jesus/papapi.git && cd ./papapi/
 python3 -m venv ./venv/
 source ./venv/bin/activate
